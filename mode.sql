de82b6d151c6d8c4e9bda7fc84f22866ff49026a
Olgun Çakabey <olguncakabey@yahoo.com>
2019-03-24 21:03:32 +0200

    Create mode.sql

---
+++ mode.sql
@@ -0,0 +1,17 @@
+select `Book-Rating`, max_count 
+from
+(
+	select `Book-Rating`, count(`Book-Rating`) as max_count from `BX-Book-Ratings`
+    where `ISBN` = '034545104X'
+	group by `Book-Rating`
+) BBR
+where max_count in 
+(
+	select max(count_val) from
+	(
+		select `Book-Rating`, count(`Book-Rating`) as count_val from `BX-Book-Ratings`
+        where `ISBN` = '034545104X'
+		group by `Book-Rating`
+	) subset
+)
+and max_count > 1
