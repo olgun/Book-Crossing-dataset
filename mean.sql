fbb757939f30761aaccfd3f48d7f7971d0d8ffc7
Olgun Çakabey <olguncakabey@yahoo.com>
2019-03-25 12:16:56 +0200

    Update mean.sql

--- mean.sql
+++ mean.sql
@@ -1,5 +1,5 @@
--- Since the question was about calculating mean for number of
--- ratings for a book therefore I took one sample book ISBN
+-- Since the question was about calculating mean for number of ratings for a book therefore I took one sample book ISBN. 
+-- Average function is used to find the mean of book ratings for desired book.
 
 select format(avg(`Book-Rating`), 2) as mean
 from `BX-Book-Ratings`
