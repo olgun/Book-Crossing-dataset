92a989d3644d10ffe460dc7963a5a138820e899d
Olgun Çakabey <olguncakabey@yahoo.com>
2019-03-25 12:25:04 +0200

    Update median.sql

--- median.sql
+++ median.sql
@@ -1,3 +1,5 @@
+-- Inner select will return the total number of rows, and then from the subset we calculate via average function the median.
+-- Also avg(subset.`Book-Rating`) and subset.row_number in (...) is used to correctly produce a median when there are an even number of records.
 SELECT avg(subset.`Book-Rating`) as median
 FROM (
   SELECT BBR.`Book-Rating`, @rownum:=@rownum+1 as `row_number`, @total_rows:=@rownum
