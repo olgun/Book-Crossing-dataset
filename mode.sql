a9f531fe79f68b72bd990948012a09358280ade2
Olgun Çakabey <olguncakabey@yahoo.com>
2019-03-25 12:31:53 +0200

    Update mode.sql

--- mode.sql
+++ mode.sql
@@ -1,3 +1,8 @@
+-- First counting the frequencies of values in the subset, by grouping on the values and using the count function. 
+-- After, selecting the max frequency value from our subtable of values and frequencies. 
+-- Then adding a filter to the query on max_count, by specifying a where clause so that only values with a frequency count in the max count 
+-- table can be returned. Add a condition on max_count to ensure it is greater than 1, so it only returns valid results when the mode exists.
+-- Lastly 'and condition' can be removed if all the values occur not only once
 select `Book-Rating` as 'Book Rating Mode', max_count as Occurance
 from
 (
